9fcd349426ba1e883ecf33080918bdc751f66f57
Benjamin Salas <balexsalas@gmail.com>
2021-09-20 20:00:39 -0600

    Consola Tarea 4

---
+++ ConsolaTarea4/ConsolaTarea4/CLSConsumoAPI.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace ConsolaTarea4
+{
+    public class CLSConsumoAPI<T>
+    {
+        Uri _direccionServicio;
+        HttpWebRequest _request;
+
+        public CLSConsumoAPI(Uri direccion)
+        {
+            if (direccion == null)
+            {
+                throw new NullReferenceException("_direccionServicio");
+            }
+            this._direccionServicio = direccion;
+        }
+
+        private void InicializarRequest()
+        {
+            this._request = WebRequest.Create(this._direccionServicio) as HttpWebRequest;
+            if (this._request == null)
+            {
+                throw new NullReferenceException("_request");
+            }
+        }
+
+        public T ObtenerResultado()
+        {
+            this.InicializarRequest();
+
+            this._request.Method = "GET";
+            return this.LeerResultado();
+        }
+
+        private T LeerResultado()
+        {
+            T resultadoOperacion;
+
+            var response = this._request.GetResponse() as HttpWebResponse;
+
+            if (response.ContentLength == 0)
+            {
+                throw new Exception("Sin contenido");
+            }
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("Se produjo una inconsistencia al invocar al servicio");
+            }
+
+            using (Stream resultado = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(resultado);
+
+                string contenido = reader.ReadToEnd();
+                resultadoOperacion = JsonSerializer.Deserialize<T>(contenido);
+            }
+
+            return resultadoOperacion;
+        }
+    }
+}
